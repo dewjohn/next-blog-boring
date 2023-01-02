3ea39cc5f7d2c00c1580068d018a4c5833f2908c
dewjohn <deweizhong@outlook.com>
2023-01-02 20:48:31 +0800

    feat:初始化next.js和nextUI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-
+"use client"
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { useServerInsertedHTML } from "next/navigation"
+import { NextUIProvider, CssBaseline } from "@nextui-org/react"
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useServerInsertedHTML(() => {
+    return <>{CssBaseline.flush()}</>
+  })
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
